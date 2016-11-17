c510ae36d8ec7f6a9e2d4206219f91f32e4e4fa2
Tologon Eshimkanov <teshimkanov@gmail.com>
2016-11-17 14:06:58 -0500

    Second refactor of gui_grid.asm file.

--- minesweeper/gui_grid.asm
+++ minesweeper/gui_grid.asm
@@ -34,42 +34,45 @@ IDM_EXIT	equ 4
 ; ___________________________________ CODE _____________________________________________________
 .code
 main PROC
-    invoke GetModuleHandle, NULL 
-    mov    hInstance,eax 
-    invoke GetCommandLine
-    mov CommandLine,eax 
-    invoke WinMain, hInstance,NULL,CommandLine, SW_SHOWDEFAULT 
-    invoke ExitProcess,eax
+    invoke	GetModuleHandle, NULL
+    mov		hInstance, eax
+    invoke	WinMain, hInstance, NULL, NULL, SW_SHOWDEFAULT 
+    invoke	ExitProcess, eax
 main ENDP
 
-WinMain PROC hInst:HINSTANCE, hPrevInst:HINSTANCE, CmdLine:LPSTR, CmdShow:DWORD 
-    LOCAL wc:WNDCLASSEX 
-    LOCAL msg:MSG 
-    LOCAL hwnd:HWND 
-    mov   wc.cbSize,SIZEOF WNDCLASSEX 
-    mov   wc.style, CS_HREDRAW or CS_VREDRAW 
-    mov   wc.lpfnWndProc, OFFSET WndProc 
-    mov   wc.cbClsExtra,NULL 
-    mov   wc.cbWndExtra,NULL 
-    push  hInst 
-    pop   wc.hInstance 
-    mov   wc.hbrBackground,COLOR_BTNFACE+1 
-    mov   wc.lpszMenuName,OFFSET MenuName 
-    mov   wc.lpszClassName,OFFSET ClassName 
-    invoke LoadIcon,NULL,IDI_APPLICATION 
-    mov   wc.hIcon,eax 
-    mov   wc.hIconSm,eax 
-    invoke LoadCursor,NULL,IDC_ARROW 
-    mov   wc.hCursor,eax 
-    invoke RegisterClassEx, addr wc 
-    invoke CreateWindowEx,WS_EX_CLIENTEDGE,ADDR ClassName, \ 
-                        ADDR AppName, WS_OVERLAPPEDWINDOW,\ 
-                        CW_USEDEFAULT, CW_USEDEFAULT,\ 
-                        200,400,NULL,NULL, hInst,NULL 
-    mov   hwnd,eax 
-    invoke ShowWindow, hwnd,SW_SHOWNORMAL 
-    invoke UpdateWindow, hwnd 
-    .WHILE TRUE 
+; _____________________________________________________________________________
+; This procedure serves two purposes:
+; 1. It initializes the main window
+; 2. It receives messages and dispatches them to related controls like buttons
+WinMain PROC hInst:HINSTANCE, hPrevInst:HINSTANCE, CmdLine:LPSTR, CmdShow:DWORD
+; _____________________________________________________________________________
+    LOCAL	wc:WNDCLASSEX 
+    LOCAL	msg:MSG 
+    LOCAL	hwnd:HWND 
+    mov		wc.cbSize, SIZEOF WNDCLASSEX 
+    mov		wc.style, CS_HREDRAW or CS_VREDRAW 
+    mov		wc.lpfnWndProc, OFFSET WndProc 
+    mov		wc.cbClsExtra, NULL 
+    mov		wc.cbWndExtra, NULL 
+    push	hInst 
+    pop		wc.hInstance 
+    mov		wc.hbrBackground, COLOR_BTNFACE+1 
+    mov		wc.lpszMenuName, OFFSET MenuName 
+    mov		wc.lpszClassName, OFFSET ClassName 
+    invoke	LoadIcon, NULL, IDI_APPLICATION 
+    mov		wc.hIcon, eax 
+    mov		wc.hIconSm, eax 
+    invoke	LoadCursor, NULL, IDC_ARROW 
+    mov		wc.hCursor, eax 
+    invoke	RegisterClassEx, addr wc 
+    invoke	CreateWindowEx, WS_EX_CLIENTEDGE, ADDR ClassName, \ 
+				ADDR AppName, WS_OVERLAPPEDWINDOW, \ 
+				CW_USEDEFAULT, CW_USEDEFAULT, \ 
+				200, 400, NULL, NULL, hInst ,NULL 
+    mov		hwnd, eax 
+    invoke	ShowWindow, hwnd,SW_SHOWNORMAL 
+    invoke	UpdateWindow, hwnd 
+    .WHILE TRUE
         invoke GetMessage, ADDR msg,NULL,0,0 
         .BREAK .IF (!eax) 
         invoke TranslateMessage, ADDR msg 
@@ -78,18 +81,23 @@ WinMain PROC hInst:HINSTANCE, hPrevInst:HINSTANCE, CmdLine:LPSTR, CmdShow:DWORD
     mov     eax,msg.wParam 
     ret 
 WinMain endp
+; _____________________________________________________________________________
 
+; _______________________________________________________________________________
+; This procedure generates grid in forms of buttons (that's the best solution
+; we have so far). This grid acts as a outer layer in the game.
 generateGrid PROC USES ecx ebx hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
+; _______________________________________________________________________________
 	mov ecx, 9	; OUTER LOOP
 MARCO:
 	push ecx	; keep outer counter for later
 	mov ecx, 9	; INNER LOOP
 	POLO:
 		push ecx
-		; the line below creates a button at (x, y) coordinates and its 20x20 size.
-		invoke CreateWindowEx,NULL, ADDR ButtonClassName,ADDR ButtonText,\ 
-				WS_CHILD or WS_VISIBLE or BS_DEFPUSHBUTTON,\ 
-				y,x,20,20,hWnd,ButtonID,hInstance,NULL
+		; 3 lines below creates a button at (x, y) coordinates and its 20x20 size
+		invoke CreateWindowEx, NULL, ADDR ButtonClassName, ADDR ButtonText, \ 
+				WS_CHILD or WS_VISIBLE or BS_DEFPUSHBUTTON, \ 
+				y, x, 20, 20, hWnd, ButtonID, hInstance, NULL
 		mov bx, x
 		add bx, 20
 		mov x, bx
@@ -104,8 +112,14 @@ MARCO:
 	loop MARCO
 	ret
 generateGrid ENDP
+; _______________________________________________________________________________
 
-WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM 
+; _____________________________________________________________
+; This procedure receives messages and acts accordingly.
+; For example, on button click, it will delete it and display
+; cell's value behind that button.
+WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
+; _____________________________________________________________
     .IF uMsg==WM_DESTROY 
         invoke PostQuitMessage,NULL 
     .ELSEIF uMsg==WM_CREATE
@@ -134,5 +148,5 @@ WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
      xor    eax,eax 
     ret 
 WndProc endp
-
+; _____________________________________________________________
 END main
